bd599a45071b8f2b4f25c3fffc1b4768a18337bd
Hackersmate-Aditya <68347060+Hackersmate-Aditya@users.noreply.github.com>
2023-09-17 20:24:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from decouple import config
+import os
 from flask import Flask, render_template, request, jsonify
+from dotenv import load_dotenv
 import openai
-
+load_dotenv()
 app = Flask(__name__)
 
-# Load the OpenAI API key from the .env file
-OPENAI_API_KEY = config('OPENAI_API_KEY')
+# Set your OpenAI API key
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route('/')
 def index():
